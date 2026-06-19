e485bec7ef6745a613eb6bb9b9472ef29690d89a
condorbox <minecraft13122001@gmail.com>
2026-06-19 12:02:51 +0200

    Print landmarks

--- main.py
+++ main.py
@@ -5,6 +5,35 @@ from mediapipe.tasks.python import vision
 
 DETECTION_THRESHOLD = 0.5
 
+# Key landmark indices for Mediapipe's face mesh
+LM = {
+    "nose_tip":        1,
+    "chin":            152,
+
+    # Mouth
+    "mouth_left":      61,
+    "mouth_right":     291,
+    "mouth_top":       13,
+    "mouth_bottom":    14,
+
+    # Left eye 
+    "left_eye_top":    159,
+    "left_eye_bottom": 145,
+    "left_eye_left":   33,
+    "left_eye_right":  133,
+
+    # Right eye
+    "right_eye_top":    386,
+    "right_eye_bottom": 374,
+    "right_eye_left":   362,
+    "right_eye_right":  263,
+
+    # Eyebrows
+    "left_eyebrow":    105,
+    "right_eyebrow":   334,
+}
+# --------------------------------------------------------------------------------------
+
 base_options = mp_python.BaseOptions(model_asset_path="face_landmarker.task")
 options = vision.FaceLandmarkerOptions(
     base_options = base_options,
@@ -35,10 +64,19 @@ while True:
     frame_index += 1
 
     if result.face_landmarks:
+        landmarks = result.face_landmarks[0]
         h, w = frame.shape[:2]
-        for landmark in result.face_landmarks[0]:  # first (only) face
+
+        for landmark in landmarks:
             x, y = int(landmark.x * w), int(landmark.y * h)
             cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
+        
+        # Print landmarks
+        for name, idx in LM.items():
+            lm = landmarks[idx]
+            x, y = int(lm.x * w), int(lm.y * h)
+            cv2.circle(frame, (x, y), 3, (0, 0, 255), -1)
+            cv2.putText(frame, name, (x + 5, y), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 1)
 
     cv2.imshow("Reaction swapper", frame)
 
@@ -48,3 +86,6 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+
